1aafdf79fa89c627101f810e546d8382d1f3d460
alpacapacapaca <haruki.y1022@iCloud.com>
2026-08-21 01:09:17 +0900

    bug追加出現場所の選定から出現率の合計をとる

--- boku no mori/Assets/Script/GameManager.cs
+++ boku no mori/Assets/Script/GameManager.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // BugData型のデータを複数入れられるListを作成
+    // BugData型の登録する虫のデータを複数入れられるListを作成
     public List<BugData> bugs;
-    // 現在のスポーンタイプを保持する変数
+    
+    // BugData型のその場に出てくる虫のデータを複数入れられるListを作成
+    public List<BugData> spawnableBugs;
+    // 現在のスポーンタイプを設定する変数
     public BugSpawnType currentSpawnType;
+    //出現率の合計値を格納する変数
+    int totalSpawnRate = 0;
+    //出現する虫のランダム値を格納する変数
+    //int raqndomValue = Random.Range(0,totalSpawnRate);
 
     void Start()
     {
@@ -16,8 +23,14 @@ public class GameManager : MonoBehaviour
             //もし現在のスポーンタイプがバグのスポーンタイプと一致する場合のみ表示
             if (bugs[i].spawnType == currentSpawnType)
             {
+                spawnableBugs.Add(bugs[i]);
                 Debug.Log(bugs[i].bugName);
             }
+        }
+        for (int i = 0; i < spawnableBugs.Count; i++)
+        {
+            totalSpawnRate += spawnableBugs[i].spawnRate;
+            Debug.Log(totalSpawnRate);
         }
     }
 
